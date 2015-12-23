e9381b4198efbd1aa1d2789da4b4ac60a13fa537
David Badura <david.badura@i22.de>
2015-12-23 16:25:29 +0100

    and again

--- build/build.sh
+++ build/build.sh
@@ -6,9 +6,8 @@ wget http://taskwarrior.org/download/task-$TASKWARRIOR.tar.gz
 gunzip task-$TASKWARRIOR.tar.gz
 tar xf task-$TASKWARRIOR.tar
 cd task-$TASKWARRIOR
-sudo apt-get install cmake build-essential uuid-dev libgnutls-dev libreadline6-dev --force-yes
-sudo cmake -DCMAKE_BUILD_TYPE=release .
-sudo make
+cmake -DCMAKE_BUILD_TYPE=release .
+make
 cd ..
 cp task-$TASKWARRIOR/src/task ./taskwarrior-$TASKWARRIOR
 sudo rm -Rf task-$TASKWARRIOR*
